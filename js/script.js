5e124ec1df2adc7dbcb90fde3f8625d59670deac
Tomasz Turbak <fronendtt@gmail.com>
2022-12-29 15:26:23 +0100

    form clearing + focus after adding a task

--- js/script.js
+++ js/script.js
@@ -73,13 +73,15 @@
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
 
-        if (newTaskContent === "") {
-            return;
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
         }
 
-        addNewTask(newTaskContent);
+        newTaskElement.focus();
     };
 
     const init = () => {

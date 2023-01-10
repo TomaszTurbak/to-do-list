c6a7ce28c733e409e6ebc418a20b89a44fb523f0
Tomasz Turbak <fronendtt@gmail.com>
2023-01-10 15:53:26 +0100

    preperation for additional buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 {
     let tasks = [];
+    let hideDoneTasks = false;
 
     const addNewTask = (newTaskContent) => {
         tasks = [
@@ -22,13 +23,29 @@
         tasks = [
             ...tasks.slice(0, taskIndex),
             {
-                ...tasks[taskIndex].done,
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
             },
             ...tasks.slice(taskIndex + 1),
         ];
 
         render();
     }
+
+    const markAllTasksDone = () => {
+        tasks = tasks.map((task) => ({
+            ...task,
+            done: true,
+        }));
+
+        render();
+    };
+
+    const toggleHideDoneTasks = () => {
+        hideDoneTasks = !hideDoneTasks;
+        render();
+    };
+
 
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
